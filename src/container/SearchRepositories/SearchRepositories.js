7abd39fec2b9a681bad12c63dfdd1f0d6ffa1d43
TomoyaIshida <tmyisd@icloud.com>
2019-01-06 21:33:07 +0900

    Add style

--- src/container/SearchRepositories/SearchRepositories.js
+++ src/container/SearchRepositories/SearchRepositories.js
@@ -3,7 +3,7 @@ import { ApolloProvider, Query } from 'react-apollo'
 import client from '../../container/client'
 import { SEARCH_REPOSITORIES } from '../../container/graphql'
 import { __importDefault } from 'tslib';
-import StarButton from '../StarButton/StarButton'
+import StarButton from '../StarButton/StarButton';
 
 const PER_PAGE = 10
 const DEFAULT_STATE = {
@@ -13,6 +13,15 @@ const DEFAULT_STATE = {
   before: null,
   query: ""
 }
+const css = {
+  fontFamily: 'Helvetica Neue Helvetica Arial Hiragino Sans ヒラギノ角ゴシック Hiragino Kaku Gothic ProN ヒラギノ角ゴ Pro W3 Roboto メイリオ Meiryo ＭＳ Ｐゴシック sans-serif',
+  width: '250px',
+  margin: '70px auto 0',
+  padding: '15px 50px 30px',
+  borderRadius: '5px',
+  boxShadow: '0 2px 5px rgba(0,0,0,0.26)'
+}
+
 class SearchRepositories extends Component {
   constructor(props) {
     super(props)
@@ -47,50 +56,52 @@ class SearchRepositories extends Component {
   }
 
   render() {
-    console.log(this.state)
     const { query, first, last, before, after } = this.state
     return (
       <ApolloProvider client={client}>
-        <form onSubmit={this.handleSubmit}>
-          <input ref={this.myRef}/>
-          <input type="submit" value="Submit" />
-        </form>
-        <Query
-          query={SEARCH_REPOSITORIES}
-          variables={{ query, first, last, before, after }}
-        >
-          {
-            ({ loading, error, data }) => {
-              if (loading) return 'Loading...'
-              if (error) return `Error! ${error.message}`
-              const search = data.search
-              const repositoryCount = search.repositoryCount
-              const title = `GitHubリポジトリ検索結果  ${repositoryCount}件`
-              return (
-                <React.Fragment>
-                  <p>{title}</p>
-                  <ul>
-                    {
-                      search.edges.map(edge => {
-                        const node = edge.node
-                        return (
-                          <li key={node.id}>
-                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
-                            &nbsp;
-                            <StarButton node={node} {...{query, first, last, after, before}}/>
-                          </li>
-                        )
-                      })
-                    }
-                  </ul>
+        <div style={css}>
+          <h1>GitHub Search</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input ref={this.myRef}/>
+            <input type="submit" value="Submit" />
+          </form>
+          <Query
+            query={SEARCH_REPOSITORIES}
+            variables={{ query, first, last, before, after }}
+          >
+            {
+              ({ loading, error, data }) => {
+                if (loading) return 'Loading...'
+                if (error) return `Error! ${error.message}`
+                const search = data.search
+                const repositoryCount = search.repositoryCount
+                const title = `GitHubリポジトリ検索結果  ${repositoryCount}件`
+                return (
+                  <React.Fragment>
+                    <p>{title}</p>
+                    <ul>
+                      {
+                        search.edges.map(edge => {
+                          const node = edge.node
+                          return (
+                            <li key={node.id}>
+                              <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                              &nbsp;
+                              <StarButton node={node} {...{query, first, last, after, before}}/>
+                            </li>
+                          )
+                        })
+                      }
+                    </ul>
 
-                  { search.pageInfo.hasPreviousPage === true ? <button onClick={this.goPrevious.bind(this, search)}>Previous</button> : null }
-                  { search.pageInfo.hasNextPage === true ? <button onClick={this.goNext.bind(this, search)}>Next</button> : null }
-                </React.Fragment>
-              )
+                    { search.pageInfo.hasPreviousPage === true ? <button onClick={this.goPrevious.bind(this, search)}>Previous</button> : null }
+                    { search.pageInfo.hasNextPage === true ? <button onClick={this.goNext.bind(this, search)}>Next</button> : null }
+                  </React.Fragment>
+                )
+              }
             }
-          }
-        </Query>
+          </Query>
+        </div>
       </ApolloProvider>
     )
   }
